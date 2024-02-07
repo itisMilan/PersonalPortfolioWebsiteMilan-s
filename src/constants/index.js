8044cbda162c6aa6cc3d9f00430f93f9b253a08f
itisMilan <abhaymilan8@gmail.com>
2024-02-07 13:49:38 +0530

    Added Portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,287 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  jobit,
+  tripguide,
+  threejs,
+  Screenshot1,
+  Screenshot2,
+  Screenshot3,
+  logo1,
+  logo2,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Front-End Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Web3 Enthusiast",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  // {
+  //   title: "React.js Developer",
+  //   company_name: "Starbucks",
+  //   icon: starbucks,
+  //   iconBg: "#383E56",
+  //   date: "March 2020 - April 2021",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "React Native Developer",
+  //   company_name: "Tesla",
+  //   icon: tesla,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2021 - Feb 2022",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  {
+    title: "Software Developer",
+    company_name: "100Devs",
+    icon: logo2,
+    iconBg: "#383E56",
+    date: "Sep 2022 - June 2023",
+    points: [
+      "      As a Full Stack Developer at 100Devs, my responsibilities include:",
+      "      Crafting and sustaining web applications utilizing React.js and associated technologies.",
+      "      Collaborating seamlessly with cross-functional teams, comprising designers, product managers, and fellow developers, to deliver top-tier products.",
+      // Implementing responsive design practices and ensuring compatibility across various browsers.
+      "      Actively participating in code reviews, offering constructive feedback to enhance the collective development process.",
+      "      During my tenure at 100Devs, I specialized in utilizing the MERN stack, engaged in regular standup sessions, contributed to open-source projects, and conducted comprehensive research on the internet.",
+      // "As a Full Stack Developer at 100Devs, I contributed to the development and maintenance of innovative web applications using the MERN stack. Working closely with a collaborative and cross-functional team that included designers and fellow developers, I contributed to the creation of high-quality and innovative digital solutions.",
+      // // "Key Responsibilities and Achievements:",
+      // // "Tech Stack: Spearheaded MERN stack development, ensuring scalable and robust solutions aligned with industry best practices.",
+      //  "Collaboration: Actively engaged in standup sessions, collaborating seamlessly with cross-functional teams, including designers and product managers.",
+      //  "Open Source: Contributed to open source projects, enhancing code quality and fostering a culture of knowledge sharing within the developer community.",
+      //  "Code Reviews: Played a crucial role in maintaining code quality through active participation in code reviews, providing constructive feedback to peers.",
+      //  "At 100Devs, I not only enhanced my technical skills but also thrived in a collaborative and innovative environment, contributing to the success of dynamic web development projects."
+      ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "MillionBytes",
+    icon: logo1,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Current",
+    points: [
+     
+      "      As a passionate Full Stack Developer at MillionBytes, I've immersed myself in the art of creating seamless web experiences.",
+      "Verified: Our Swedish Connection",
+      "      Our mission at MillionBytes intertwines with Verified, a trailblazing company hailing from Sweden. Verified specializes in simplifying and automating processes for AML & KYC, electronic signing, and digital identification. As their trusted partner, we integrate their services seamlessly into other companies' workflows. Our role? Crafting bespoke flows that align with each client's unique needs.",
+      "      The Flow Architects",
+      "At MillionBytes, we're more than developers—we're flow architects." ,
+      "Our canvas expands as we bridge the gap between Verified's offerings and the diverse requirements of our clients.",
+      // "      Curious Explorer",
+      // " Beyond code, I'm an avid researcher. The internet is my treasure trove, where I uncover gems of knowledge, explore emerging technologies, and contribute to open-source projects. My standup sessions involve not just updates but also insights gained from the vast digital expanse.",
+      // We meticulously design data pipelines, orchestrate APIs, and ensure smooth interactions.
+   ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Got An Idea",
+    description:
+      "gotAnIdea is an innovative platform powered by Next.js and secured with Clerk authentication. It enables visionaries to share creative concepts, engage in a thriving community, and collaborate on ideas. Real-time updates and seamless communication enhance the user experience. 🚀",
+    tags: [
+      {
+        name: "Next JS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Clerk",
+        color: "white-text-gradient",
+      },{
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Screenshot1,
+    source_code_link: "https://github.com/itisMilan/gotAnIdea",
+  },
+  {
+    name: "PromptPal",
+    description:
+      "A web application built with Next.js, incorporating seamless authentication via NextAuth. Users can share creative prompts, fostering collaborative inspiration within the community. The platform ensures a responsive and engaging user experience while prioritizing data security through NextAuth authentication. 🌟",
+    tags: [
+      {
+        name: "Next JS",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "NextAuth",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Screenshot2,
+    source_code_link: "https://github.com/itisMilan/NextJS-AI-Prompt-App",
+  },
+  {
+    name: "CinePhileflix( Web App)",
+    description:
+      "A web application inspired by streaming platforms built by react js, focusing on trailers for a curated list of movies. Due to rights limitations, full movies are not available, but users can enjoy exciting previews.",
+    tags: [
+      {
+        name: "react js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tmdb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: Screenshot3,
+    source_code_link: "https://github.com/itisMilan/CinePhileflix",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
