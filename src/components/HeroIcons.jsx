89de08e912b7ccb72e5e7b020d2f688e67a2b592
itisMilan <abhaymilan8@gmail.com>
2024-02-11 13:04:49 +0530

    Added Icons For The Social Media Links

---
+++ src/components/HeroIcons.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { iconLinks } from "../constants";
+function HeroIcons({ Icon, name }) {
+  return(
+       <>
+       <div className=" flex w-full  justify-center">
+
+      <ul className="flex justify-center    w-full md:w-1/6 ">
+        {iconLinks.map((item)=>(
+            <li
+             key={item.id}
+             className="px-2"
+            >
+                <a href="#">
+
+                       <img width="44" height="44" src={item.id} alt="github"/>
+                </a>
+                </li> 
+            )
+            )
+        }
+       
+      </ul>
+        </div>
+       </>
+  ) 
+}
+export default HeroIcons;
